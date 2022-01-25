1a0cb5d7b075dc6e90d0c6a1165c2756799ac1f0
Daniel Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2022-01-25 13:52:19 +0100

    fix encoding error on german experiment messages

--- common/experiment_messages.py
+++ common/experiment_messages.py
@@ -4,5 +4,5 @@ import yaml
 class ExperimentMessages(DotMap):
     def __init__(self, language, encoding):
         filepath = f'languages/{language}/experiment_messages.yaml'
-        with open(filepath, 'r') as stream:
+        with open(filepath, mode='r', encoding=encoding) as stream:
             super().__init__(yaml.safe_load(stream))
--- wmc.py
+++ wmc.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.2.3),
-    on January 25, 2022, at 13:30
+    on January 25, 2022, at 13:50
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
